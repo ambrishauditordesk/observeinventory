13a0de2b3e024096cac81e7d840044dea095d93a
20-anik <drjben10@gmail.com>
2021-03-02 19:16:32 +0530

    Updated db connection password for server.

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
     $dbuser = "root";
-    $dbpass = "anik98";
+    $dbpass = "E7rH%h#Q@8t9y+M";
     $dbname= "audit";
 // Create connection
     $con = new mysqli($servername, $dbuser, $dbpass , $dbname);
