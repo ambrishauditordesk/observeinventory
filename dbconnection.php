ff079c805ce066734af3c0d5adc3b5627dcaeb37
Sujoy Banerjee <banerjeesujoy007@gmail.com>
2021-06-28 13:21:47 +0530

    DB pass change for AWS

--- dbconnection.php
+++ dbconnection.php
@@ -1,7 +1,7 @@
 <?php
     $servername = "localhost";
     $dbuser = "root";
-    $dbpass = "root";
+    $dbpass = "yourfirmaudit@1";
     $dbname= "audit";
 // Create connection
     $con = new mysqli($servername, $dbuser, $dbpass , $dbname);
